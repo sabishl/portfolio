589c41271df6864de37d6c300fcb2327f7d586dd
Sabish <144409979+sabishl@users.noreply.github.com>
2024-12-01 13:21:00 +0530

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,10 +7,15 @@ export default function Contant(){
                         <p className='pb-5  text-red-600'>
                             If you want to disscuss more detail, please contact me
                         </p>
+                       
                        <p className="py-2"><span className="font-bold"> Email: </span> lsabish2001@gmail.com</p>
                        <p className="py-2"><span className="font-bold"> Phone No: </span> 9095399164</p>
                     </div>
-                
+                    <div className=' flex flex-col text-center p-5 font-hero-font font-bold text-3xl'>
+           <h1>
+             <span className='text-red-500'> L</span>EARN UNTIL YOU DIE
+           </h1>
+      </div>
            
         </section>
     );
